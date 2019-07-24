6de30e543037260b1747c7a5a1dd3f82acc88704
Joseph Zheng <370774511@qq.com>
2019-07-24 16:17:10 -0700

    AITweetGenerator v2.0: Now it can replace names of users in user mentions and quoted tweets

--- tweet_generator.py
+++ tweet_generator.py
@@ -31,8 +31,8 @@ def main(infile, outfile, size):
     # silence tensorflow
     tf.logging.set_verbosity(tf.logging.ERROR)
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-    print('Generating general fake texts...')
+    # generate tweets
+    print('Generating general fake tweets...')
     start_time = time.time()
     name_dict = {}
     count = 0
@@ -45,7 +45,7 @@ def main(infile, outfile, size):
                     s = json.loads(next(fin))
                     if 'text' in s.keys():
                         tweet.append(s)
-                except ValueError:
+                except (StopIteration, ValueError) as error:
                     print('Reached end of file!')
                     flag = False
                     break
@@ -60,21 +60,64 @@ def main(infile, outfile, size):
             ftext = list(itertools.chain.from_iterable(ftext))
             for s, t in zip(tweet, ftext[:len(tweet)]):
                 s['text'] = t
-                if s['user']['id'] not in name_dict.keys():
-                    id = s['user']['id']
+                uid = s['user']['id']
+                if uid not in name_dict.keys():
                     name = ng.gen_two_words(split=' ', lowercase=False)
-                    screenname = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
-                    name_dict[id] = {'name': name, 'screen_name': screenname}
+                    screen_name = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
+                    name_dict[uid] = {'name': name, 'screen_name': screen_name}
                     s['user']['name'] = name
-                    s['user']['screen_name'] = screenname
+                    s['user']['screen_name'] = screen_name
                 else:
-                    id = s['user']['id']
-                    s['user']['name'] = name_dict[id]['name']
-                    s['user']['screen_name'] = name_dict[id]['screen_name']
+                    s['user']['name'] = name_dict[uid]['name']
+                    s['user']['screen_name'] = name_dict[uid]['screen_name']
+                for i in range(len(s['entities']['user_mentions'])):
+                    mid = s['entities']['user_mentions'][i]['id']
+                    if mid not in name_dict.keys():
+                        name = ng.gen_two_words(split=' ', lowercase=False)
+                        screen_name = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
+                        name_dict[mid] = {'name': name, 'screen_name': screen_name}
+                        s['entities']['user_mentions'][i]['name'] = name
+                        s['entities']['user_mentions'][i]['screen_name'] = screen_name
+                    else:
+                        s['entities']['user_mentions'][i]['name'] = name_dict[mid]['name']
+                        s['entities']['user_mentions'][i]['screen_name'] = name_dict[mid]['screen_name']
+                if 'quoted_status' in s.keys():
+                    qid = s['quoted_status']['user']['id']
+                    if qid not in name_dict.keys():
+                        name = ng.gen_two_words(split=' ', lowercase=False)
+                        screen_name = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
+                        name_dict[qid] = {'name': name, 'screen_name': screen_name}
+                        s['quoted_status']['user']['name'] = name
+                        s['quoted_status']['user']['screen_name'] = screen_name
+                    else:
+                        s['quoted_status']['user']['name'] = name_dict[qid]['name']
+                        s['quoted_status']['user']['screen_name'] = name_dict[qid]['screen_name']
+                    for i in range(len(s['quoted_status']['entities']['user_mentions'])):
+                        mid = s['quoted_status']['entities']['user_mentions'][i]['id']
+                        if mid not in name_dict.keys():
+                            name = ng.gen_two_words(split=' ', lowercase=False)
+                            screen_name = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
+                            name_dict[mid] = {'name': name, 'screen_name': screen_name}
+                            s['quoted_status']['entities']['user_mentions'][i]['name'] = name
+                            s['quoted_status']['entities']['user_mentions'][i]['screen_name'] = screen_name
+                        else:
+                            s['quoted_status']['entities']['user_mentions'][i]['name'] = name_dict[mid]['name']
+                            s['quoted_status']['entities']['user_mentions'][i]['screen_name'] = name_dict[mid]['screen_name']
+                    if 'extended_tweet' in s['quoted_status'].keys():
+                        for i in range(len(s['quoted_status']['extended_tweet']['entities']['user_mentions'])):
+                            mid = s['quoted_status']['extended_tweet']['entities']['user_mentions'][i]['id']
+                            if mid not in name_dict.keys():
+                                name = ng.gen_two_words(split=' ', lowercase=False)
+                                screen_name = ''.join(name.split(' ')).lower() + ng.gen_year(1900, 2020) + ng.gen_birthday()
+                                name_dict[mid] = {'name': name, 'screen_name': screen_name}
+                                s['quoted_status']['extended_tweet']['entities']['user_mentions'][i]['name'] = name
+                                s['quoted_status']['extended_tweet']['entities']['user_mentions'][i]['screen_name'] = screen_name
+                            else:
+                                s['quoted_status']['extended_tweet']['entities']['user_mentions'][i]['name'] = name_dict[mid]['name']
+                                s['quoted_status']['extended_tweet']['entities']['user_mentions'][i]['screen_name'] = name_dict[mid]['screen_name']
                 line = json.dumps(s)
                 fout.write(line + '\n')
             print('Processed records:', count)
-
     print('Generate time: {} seconds'.format(time.time() - start_time))
 
 
